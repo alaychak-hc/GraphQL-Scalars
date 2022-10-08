ca9b945475d50ba678047c601d8c7240f840f478
Andrew Laychak <90005942+alaychak-hc@users.noreply.github.com>
2022-10-08 17:20:17 -0400

    fix: :bug: fix for custom types

--- packages/scalars/src/scalars/Ant Design File Upload.ts
+++ packages/scalars/src/scalars/Ant Design File Upload.ts
@@ -5,7 +5,7 @@
     Email: ALaychak@harriscomputer.com
 
     Created At: 05-11-2022 11:57:10 PM
-    Last Modified: 05-12-2022 02:03:02 AM
+    Last Modified: 10-08-2022 05:17:24 PM
     Last Updated By: Andrew Laychak
 
     Description: Scalar for the upload file from an Ant Design File Upload component
@@ -25,15 +25,12 @@ const AntDesignFileUploadScalar = new GraphQLScalarType({
   name: 'AntDesignFileUploadScalar',
   description:
     'The upload scalar that represents a file from an Ant Design File Upload component',
-  parseValue(value: { promise: unknown }) {
-    if ('originFileObj' in value) return value.promise;
+  parseValue(value: { originFileObj?: { promise: unknown } }) {
+    if ('originFileObj' in value) return value;
     throw new GraphQLError('Ant Design - upload value invalid.');
   },
   parseLiteral(ast) {
     throw new GraphQLError('Ant Design - upload literal unsupported.', ast);
-  },
-  serialize() {
-    throw new GraphQLError('Ant Design - upload serialization unsupported.');
   },
 });
 // #endregion
